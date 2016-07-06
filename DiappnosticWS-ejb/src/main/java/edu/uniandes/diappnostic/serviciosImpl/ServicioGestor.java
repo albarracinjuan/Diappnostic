1dba8d5a7bfc0b3eaea60d2ff9890df9604cd36e
Daniel Arevalo <da.arevalo@uniandes.edu.co>
2016-07-05 23:08:10 -0500

    Implementando colas

--- DiappnosticWS-ejb/src/main/java/edu/uniandes/diappnostic/serviciosImpl/ServicioGestor.java
+++ DiappnosticWS-ejb/src/main/java/edu/uniandes/diappnostic/serviciosImpl/ServicioGestor.java
@@ -1,11 +1,14 @@
 package edu.uniandes.diappnostic.serviciosImpl;
 
 import java.util.List;
-import javax.ejb.Stateless;
-import javax.inject.Inject;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
 import edu.uniandes.diappnostic.dto.EpisodioDto;
 import edu.uniandes.diappnostic.persistencia.IEpisodioDAO;
+import edu.uniandes.diappnostic.queue.AgregarEpisodioHilo;
 import edu.uniandes.diappnostic.servicios.IServicioGestor;
 
 /**
@@ -18,18 +21,26 @@ public class ServicioGestor implements IServicioGestor {
 	/**
 	 * 
 	 */
-	@Inject
+	@EJB
 	private IEpisodioDAO episodioDao;
 	
-
+	private static Logger log = Logger.getLogger(ServicioGestor.class.getName());
 	/**
 	 * registra un episodio en el sistema
 	 * @param episodio informacion del episodio
 	 */
 	@Override
 	public void registrarEpisodio(EpisodioDto episodioDto) {
+		System.out.println("========= 3. Registrando episodio inicio =========");
+		Thread agregarEpisodioHilo = new Thread(new AgregarEpisodioHilo(episodioDto));
+		agregarEpisodioHilo.start();
+		try {
+			agregarEpisodioHilo.join();
+		} catch (InterruptedException e) {
+			log.log(Level.WARNING, e.getMessage());
+		}
+		System.out.println("========= 3. Registrando episodio fin =========");
 		
-		episodioDao.registrarEpisodio(episodioDto);
 	}
 
 	/**
@@ -42,4 +53,5 @@ public class ServicioGestor implements IServicioGestor {
 	public List<EpisodioDto> consultarEpisodios(long identificacion) {
 		return episodioDao.consultarEpisodios(identificacion);
 	}
+	
 }
